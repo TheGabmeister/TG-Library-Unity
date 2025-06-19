19e15ff9854031d3cbd1b0c26c77b9ec500ed0b9
TheGabmeister <thegabmeister@protonmail.com>
2025-06-20 07:08:10 +0800

    Update SImpleEventSystem

--- SimpleEventSystem/Events.cs
+++ SimpleEventSystem/Events.cs
@@ -1,47 +1,54 @@
-using SimpleEventSystem;
+using System;
 using UnityEngine;
 
-/*
- * When there are too many events, you need to start categorizing each event to a separate class as shown in the example below.
- * An alternative approach is to use robust naming scheme (Ex. Player_Jumped) and use partial classes.
- */
+// A simple event system based on https://playable.design/a-simple-event-system-for-unity
+// This approach, while simple, allocates memory on the heap. A better (but more verbose) approach
+// is to allocate on the stack. Example: https://github.com/adammyhre/Unity-Event-Bus
 
 namespace SimpleEventSystem
 {
-    public static class Events
+    public static class GameStateEvents
     {
-        public static readonly PlayerEvents Player;
-        public static readonly ItemEvents Item;
+        public static readonly GameEvent Started = new();
+        public static readonly GameEvent Restarted = new();
     }
 
-    // You can put this in a separate script
-    public class PlayerEvents
+    public static class PlayerEvents
     {
-        public static readonly GameEvent OnJump = new();
-        public static readonly GameEvent<float> OnDamage = new();
+        public static readonly GameEvent Died = new();
+        public static readonly GameEvent CheckpointPassed = new();
     }
 
-    public class ItemEvents
+    public static class MusicEvents
     {
-        public static readonly GameEvent<GameObject> OnPickup = new();
+        public static readonly GameEvent<AudioClip> Play = new();
+        public static readonly GameEvent Pause = new();
+        public static readonly GameEvent Stop = new();
+    }
+
+    public static class SfxEvents
+    {
+        public static readonly GameEvent<AudioClip> Play = new();
+    }
+
+    public class GameEvent
+    {
+        private event Action action = delegate { };
+        public void Raise() => action?.Invoke();
+        public void Sub(Action subscriber) => action += subscriber;
+        public void Unsub(Action subscriber) => action -= subscriber;
+    }
+
+    public class GameEvent<T>
+    {
+        private event Action<T> action;
+        public void Raise(T param) => action?.Invoke(param);
+        public void Sub(Action<T> subscriber) => action += subscriber;
+        public void Unsub(Action<T> subscriber) => action -= subscriber;
     }
 }
 
-/* Alternative approach using partial classes with event naming scheme.
- * We use underscores to help Intellisense filter the events based on their category.
-
-// PlayerEvents.cs
-public static partial class Events
-{
-    public static readonly GameEvent Player_Jumped = new();
-    public static readonly GameEvent<float> Player_Damaged = new();
-    
-}
-
-// ItemEvents.cs
-public static partial class Events
-{
-    public static readonly GameEvent<GameObject> Item_Pickedup = new();
-}
-
-*/
+// Example usage
+// using SimpleEventSystem;
+// PlayerEvents.Died.Sub(OnPlayerDied);
+// MusicEvents.Play.Raise(audioClip);
