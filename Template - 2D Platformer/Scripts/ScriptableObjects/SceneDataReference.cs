10b34faebc86b060205454e5bb3a10579a626a49
Gabriel Enriquez <thegabmeister@protonmail.com>
2026-05-29 18:27:27 +0800

    Delete template

--- Template - 2D Platformer/Scripts/ScriptableObjects/SceneDataReference.cs
+++
@@ -1,9 +0,0 @@
-namespace ScriptableObjectArchitecture
-{
-    [System.Serializable]
-    public sealed class SceneDataReference : BaseReference<SceneData, SceneDataVariable>
-    {
-        public SceneDataReference() : base() { }
-        public SceneDataReference(SceneData value) : base(value) { }
-    }
-}
